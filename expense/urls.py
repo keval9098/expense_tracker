93e8f16bf633f6f7b2e3414f36fdad9a2f98eae1
keval <kevalkant9098@gmail.com>
2020-03-03 22:45:54 +0530

    login/logout functionality

--- expense/urls.py
+++ expense/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<int:question_id>/', views.new_index, name='index_two'),
     path('signup', views.signup, name='signup'),
     path('search', views.search, name='search'),
+    path('login', views.login, name='search'),
+    path('logout', views.logout, name='search'),
 ]
--- expense/views.py
+++ expense/views.py
@@ -4,6 +4,9 @@ from . models import spent, User
 import datetime
 from django.db.models import Sum
 from .forms import Add_form
+from django.contrib.auth.models import auth, User
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
@@ -12,28 +15,33 @@ from . models import spent
 
 
 def index(request):
-    if request.method == 'POST':
-        a=spent()
-        a.user_name_id=1
-        a.spent_on=request.POST.get('spent_on')
-        a.reason=request.POST.get('reason')
-        a.amount=request.POST.get('amount')
-        a.time=datetime.datetime.now()
-        a.save()
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            a=spent()
+            a.user_name_id=request.user.id
+            a.spent_on=request.POST.get('spent_on')
+            a.reason=request.POST.get('reason')
+            a.amount=request.POST.get('amount')
+            a.time=datetime.datetime.now()
+            a.save()
        
             
-        return redirect('/')
+            return redirect('/')
         
+        else:
+            latest_question_list = spent.objects.filter(user_name=request.user.id)
+            amount_sum=spent.objects.all().filter(user_name=request.user.id).aggregate(Sum('amount')).get('amount__sum', 0.00)
+            template = loader.get_template('expense/index.html')
+            context = {
+                'latest_question_list': latest_question_list,
+                'amount_sum': amount_sum,
+            }
+        
+            return HttpResponse(template.render(context, request))
     else:
-        latest_question_list = spent.objects.filter(user_name='1')
-        amount_sum=spent.objects.all().aggregate(Sum('amount')).get('amount__sum', 0.00)
-        template = loader.get_template('expense/index.html')
-        context = {
-            'latest_question_list': latest_question_list,
-            'amount_sum': amount_sum,
-        }
-        
-        return HttpResponse(template.render(context, request))
+        return redirect('/login')
+
+    
     
 def new_index(request, question_id):
     new=spent.objects.filter(id=question_id).delete()
@@ -42,12 +50,17 @@ def new_index(request, question_id):
 
 def signup(request):
     if request.method=="POST":
-        b=User()
-        b.username=request.POST.get('username')
-        b.password=request.POST.get('password1')
-        b.save()
-        return redirect('/')   
+        username=request.POST['username']
+        password=request.POST['password']
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        email=request.POST['email']
+
+        user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=email)
+        user.save()
+        return redirect('/login')
     else:
+
         return render(request, 'expense/signup.html')
 def search(request):
     if request.method=='POST':
@@ -61,3 +74,24 @@ def search(request):
         }
         
         return HttpResponse(template.render(context, request))
+def login(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('/login')
+
+    else:
+        return render(request, 'expense/login.html')
+
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/login')
